7e0b83e6e4315e32456a012cea26107ad9c399d3
Parag <parag.yawale@techforceglobal.com>
2023-08-09 17:17:39 +0530

    bhau

---
+++ Test Suites/ReqRes/GetUser_TDD_Suite.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>GetUser_TDD_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ed550129-f401-4012-9a09-7285c044a2ae</testSuiteGuid>
+   <testCaseLink>
+      <guid>e9c994f5-4501-48c4-971b-e0cc18795205</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReqRes_TDD/GetUser_TDD_TC</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>d1948c0c-0a5c-4254-87be-ff48c301012a</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/TDD_Exl/ID_Numbers</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>d1948c0c-0a5c-4254-87be-ff48c301012a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>UserId</value>
+         <variableId>03cce7f5-f20a-4886-8bf8-32191275b8a9</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
